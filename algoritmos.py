3c2a20b3d1af07a6e12daa8b54c4abeae25cbcd8
nilbsongalindo <nilbsonla@gmail.com>
2018-07-25 23:11:54 -0300

    Implementação do Insertion Sort e do Selection Sort

--- algoritmos.py
+++ algoritmos.py
@@ -27,7 +27,6 @@ def SelectionSort(lista):
 def main():
 	
 	teste = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]
-	teste2 = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 
 
 	print('Lista original: %s' % str(teste))
@@ -36,6 +35,11 @@ def main():
 
 	print('Lista ordenada pelo Selection Sort: %s '% str(SelectionSort(teste)))
 
+	"""
+	best_case = [0, 4, 10, 20, 55, 63, 78, 90, 103, 120, 150]
+	worst_case = [150, 120, 103, 90, 78, 63, 55, 20, 10, 4, 0]
+  """
+  
 if __name__ == '__main__':
 	main()
 
